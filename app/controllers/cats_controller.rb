8c9eccff71e4c598cdfa2c8329162a82d38c5462
aga <hipo.beaver@gmail.com>
2014-10-08 15:29:04 +0200

    without scaffold

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,9 +1,14 @@
 class CatsController < ApplicationController
+  #before an action is called, assign the @cat variable
+  before_filter :assign_cat, only: [:show, :destroy] #only the show action
+ #we only need it for the show and destroy actions
+
   def index
     @cats = Cat.all
   end
 
   def show
+    #@cat variable is already initialized (in private)
   end
 
   def new
@@ -21,9 +26,42 @@ class CatsController < ApplicationController
     end
   end
 
+
+  def search
+      #@cats = Cat.where(name: search_term) it will look for the specific name, 
+      #query the DB to get all the cats that match name
+      search_term = params[:name]
+
+      if search_term != ''
+      @cats = Cat.where('name LIKE ?', "%#{search_term}%")
+      else
+      @cats = []
+    end
+  #or if search_term == '' || search_term.nil?
+  #@cats = []
+  #else
+  #@cats = Cat.where('name LIKE ?', "%#{search_term}%")
+
+
+
+  end
+
+  def destroy
+    @cat.destroy
+    redirect_to cats_url
+  end
+
   private
+  #filterinng bad params from a form
 
   def cat_params
     params.require(:cat).permit(:name, :birth_date)
   end
+
+  def assign_cat
+    @cat = Cat.find(params[:id])
+  end
+
+  #DELETE/cats/:id
+  
 end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,6 @@
 class Cat < ActiveRecord::Base
+
+	validates :name, presence: true
+	validates :birth_date, presence: true
+
 end
